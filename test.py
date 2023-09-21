cb7e0f02fd347503b3b037f6c2baed524e613609
MateusRomani <mateus.romani@autaza.com>
2023-09-21 19:41:38 -0300

    rotating according to the keyboard

--- test.py
+++ test.py
@@ -9,9 +9,12 @@ window_height = 600
 # Angle for petal rotation (initially set to 0)
 rotation_angle = 0.0
 
+# Angle increment for keyboard rotation
+rotation_increment = 1.0  # You can adjust the rotation speed here
+
 
 def draw_petals():
-    global rotation_angle  # Use the global rotation_angle variable
+    global rotation_angle
     draw_circle_with_rotation(0.25, 0.25, 0.20, 100, 1.0, 0.0, 0.0, rotation_angle)
     draw_circle_with_rotation(0.25, -0.25, 0.20, 100, 1.0, 0.0, 0.0, rotation_angle)
     draw_circle_with_rotation(-0.25, 0.25, 0.20, 100, 1.0, 0.0, 0.0, rotation_angle)
@@ -21,40 +24,27 @@ def draw_petals():
     draw_circle_with_rotation(0.35, 0.0, 0.20, 100, 1.0, 0.0, 0.0, rotation_angle)
     draw_circle_with_rotation(-0.35, 0.0, 0.20, 100, 1.0, 0.0, 0.0, rotation_angle)
 
-    # Increment the rotation angle for the next frame
-    rotation_angle += 1.0  # You can adjust the rotation speed here
-
 
 def draw_stalk():
-    # Set the color for the stalk (green)
-    glColor3f(0.0, 1.0, 0.0)
-
-    # Define the stalk's dimensions
+    glColor3f(0.0, 1.0, 0.0)  # Set the color for the stalk (green)
     x_left = -0.05
     x_right = 0.05
     y_bottom = -1.0
     y_top = -0.25
-
-    # Draw the stalk as a rectangle
     glRectf(x_left, y_bottom, x_right, y_top)
 
 
 def draw_circle_with_rotation(c_x, c_y, rad, n, red, green, blue, angle_degrees):
-    # Set the color for the circle
-    glColor3f(red, green, blue)
-
-    # Define the circle's center and radius
+    glColor3f(red, green, blue)  # Set the color for the circle
     center_x, center_y = c_x, c_y
     radius = rad
-    num_segments = n  # Number of line segments to approximate the circle
+    num_segments = n
 
-    # Apply rotation to the petal positions
-    angle_radians = math.radians(angle_degrees)
+    angle_radians = math.radians(angle_degrees)  # Convert angle to radians
     rotated_x = center_x * math.cos(angle_radians) - center_y * math.sin(angle_radians)
     rotated_y = center_x * math.sin(angle_radians) + center_y * math.cos(angle_radians)
 
-    # Draw the circle using line segments with rotation
-    glBegin(GL_POLYGON)
+    glBegin(GL_POLYGON)  # Draw the circle using line segments with rotation
     for i in range(num_segments):
         theta = 2.0 * math.pi * i / num_segments
         x = rotated_x + radius * math.cos(theta)
@@ -66,11 +56,9 @@ def draw_circle_with_rotation(c_x, c_y, rad, n, red, green, blue, angle_degrees)
 def display():
     glClear(GL_COLOR_BUFFER_BIT)
 
-    # Set up the projection matrix
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
 
-    # Maintain the aspect ratio while setting up the projection matrix
     aspect_ratio = window_width / window_height
     if aspect_ratio > 1:
         glOrtho(-1 * aspect_ratio, 1 * aspect_ratio, -1, 1, -1, 1)
@@ -82,7 +70,23 @@ def display():
     draw_stalk()
     draw_petals()
     draw_circle_with_rotation(0.0, 0.0, 0.35, 100, 1.0, 1.0, 1.0, 0.0)
+
     glutSwapBuffers()
+
+
+def keyboard(key, x, y):
+    global rotation_angle
+    if key == b" ":
+        # Reset rotation when spacebar is pressed
+        rotation_angle = 0.0
+    elif key == GLUT_KEY_RIGHT:
+        # Rotate petals to the right when the right arrow key is pressed
+        rotation_angle += rotation_increment
+    elif key == GLUT_KEY_LEFT:
+        # Rotate petals to the left when the left arrow key is pressed
+        rotation_angle -= rotation_increment
+
+    glutPostRedisplay()  # Trigger a redraw
 
 
 def main():
@@ -92,7 +96,8 @@ def main():
     glutCreateWindow("Rotating Petals")
 
     glutDisplayFunc(display)
-    glutIdleFunc(display)  # Call display function continuously
+    glutIdleFunc(display)
+    glutSpecialFunc(keyboard)  # Register the keyboard function for special keys
     glutMainLoop()
 
 
